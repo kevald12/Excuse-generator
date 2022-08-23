60326d864549d8fa8cec1fcb75c3e8d9d1e2376e
kevald12 <kealable@gmail.com>
2022-08-23 02:25:33 +0000

    Project finished

--- src/app.js
+++ src/app.js
@@ -5,32 +5,52 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = [
-  "El perro",
-  "El gato",
-  "Mi prima",
-  "un oficial",
-  "mi novia",
-  "un pintor"
-];
-let action = [
-  "comio",
-  "daño",
-  "se llevo",
-  "rompio",
-  "robo",
-  "ensucio",
-  "perdio"
-];
-
-let what = ["Mi carro", "Mi moto", "Mis llaves", "Mi tarea"];
-
-let when = ["ayer", "hoy", "el finde"]
-
-
-window.onload = function() {
-
- generadorDeExcusas(who,what,when,action){
-  awho = Math.floor(Math.random() * who.length);
-}
+//function onload() {
+//let excusa = document.getElementById("excusas");
+//excusa.innerHTML = generadorDeExcusas(who, what, what, when, action);
+//}
+window.onload = () => {
+  //write your code here
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excusas").innerHTML = generadorDeExcusas();
+  });
 };
+
+let generadorDeExcusas = () => {
+  let who = [
+    "el perro",
+    "el gato",
+    "mi prima",
+    "un oficial",
+    "mi novia",
+    "un pintor"
+  ];
+  let action = [
+    "se comio",
+    "me daño",
+    "se llevo",
+    "me rompio",
+    "me robo",
+    "me ensucio",
+    "me perdio"
+  ];
+
+  let what = ["mi carro", "mi moto", "mis llaves", "mi tarea"];
+
+  let when = ["Ayer", "Hoy", "El finde"];
+  let pWho = Math.floor(Math.random() * who.length);
+  let pWhen = Math.floor(Math.random() * when.length);
+  let pWhat = Math.floor(Math.random() * what.length);
+  let pAction = Math.floor(Math.random() * action.length);
+
+  return (
+    when[pWhen] + " " + who[pWho] + " " + action[pAction] + " " + what[pWhat]
+  );
+};
+//window.onload = () => {
+//document.querySelector("#btn").addEventListener("click", () => {
+//document.querySelector("#excusas").innerHTML = generadorDeExcusas();
+//});
+//};
+
+window.onload = onload;
